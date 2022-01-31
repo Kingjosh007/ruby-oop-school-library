af288178b2a8db6af776f785f90dc4c392ce58ba
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-31 15:28:03 +0100

    Add id property to Person class

--- person.rb
+++ person.rb
@@ -3,6 +3,7 @@ class Person
   attr_accessor :name, :age
   
   def initialize(age, name="Unknown", parent_permission=true)
+    @id = rand(1..10000)
     @age = age
     @name = name
     @parent_permission = parent_permission
