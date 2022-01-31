475cbcda83fb0aeb82966df34fefea1b5e86c547
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-31 16:02:17 +0100

    Create classes Student and Teacher

--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
 require_relative 'person'
 require_relative 'student'
+require_relative 'teacher'
 
 a = Person.new(23, "John")
 b = Student.new(24, "Classroom 1", "King");
+c = Teacher.new(38, "Biology", "Mr. Smith");
+
+c
+c.can_use_services?
--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require_relative 'person'
 
 class Student < Person
+    attr_accessor :classroom
 
   def initialize(age, classroom, name="Unknown", parent_permission=true)
     super(age, name, parent_permission)
