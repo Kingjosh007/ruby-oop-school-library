cb2bb677cf37a1d63efc4e5515dc143159b00226
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-01 22:38:12 +0100

    Create method 'validate_name' in class Person

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
+require_relative 'corrector'
+
 class Person
-  attr_reader :id
+  attr_reader :id, :corrector
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission)
@@ -7,10 +9,15 @@ class Person
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @corrector = Corrector.new()
   end
 
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 
   private
