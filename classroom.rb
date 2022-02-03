3e916b9a96b62db9426819678d1d5b190cc364e9
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-03 11:10:52 +0100

    Create add_student method in Classroom class

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,12 @@
 class Classroom
+  attr_reader :students
   attr_accessor :label
-  def initialize(label)
+  def initialize(label, students = [])
     @label = label
+    @students = students
+  end
+
+  def add_student(student)
+    @students << student unless @students.include?(student)
   end
 end
