860c2816b0759045403c1f3c97b74bdd01062ddc
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-04 16:11:24 +0100

    Introduced bug in main.rb

--- main.rb
+++ main.rb
@@ -9,16 +9,18 @@ require_relative 'rental'
 b = Student.new(24, 'Classroom 1', 'King')
 c = Teacher.new(38, 'Biology', 'Mr. Smith')
 myco = Corrector.new
-pers = Person.new(35, 'Benoit Delafontaine')
+pers = Person.new("35", 'Benoit Delafontaine', false)
 book1 = Book.new('The Hobbit', 'J.R.R. Tolkien')
 book2 = Book.new('The Lord of the Rings', 'J.R.R. Tolkien')
 # book3 = Book.new('The Catcher in the Rye', 'J.D. Salinger')
 # book4 = Book.new('The Grapes of Wrath', 'John Steinbeck')
 
-myco.correct_name('KingJoChewa')
+
+# myco.correct_name('KingJoChewa')
 p pers
 pers.validate_name
 p pers
+p.can_use_services?
 
 # p b
 # b.rent_book(book1)
