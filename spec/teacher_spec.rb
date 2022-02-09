de406af745cbd9a08cf439c442cf3b9134a3f2de
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-09 15:31:36 +0100

    Write tests for Teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../teacher'
+
+describe Teacher do
+    context 'When testing the Teacher class' do 
+
+        it 'A Teacher is also a Person (inheritance)' do
+            teacher = Teacher.new(40, 'Web Development', 'Paul Parker')
+            expect(teacher).to be_kind_of(Person)
+        end
+
+        it 'A Teacher can always use services' do 
+            teacher1 = Teacher.new(40, 'Web Development', 'Paul Parker')
+            teacher2 = Teacher.new(44, 'Ruby', 'Tim Apollo')
+            teacher3 = Teacher.new(38, 'Whatever', 'Apollo Timothy', false)
+            teach_array = [teacher1, teacher2, teacher3]
+
+            expect(teach_array.all? { |teacher| teacher.can_use_services? }).to be true
+        end
+    end
+end
