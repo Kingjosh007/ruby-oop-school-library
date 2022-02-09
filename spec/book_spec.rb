35a403491e8208fe83e024a6ca2bccb3d0a6d312
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-09 15:35:04 +0100

    Fix Rubocop errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,32 +1,31 @@
 require_relative '../book'
 
-describe Book do 
-    context 'When testing the Book class' do 
-       
-       it 'A newly created book has a title of type String' do 
-          bk = Book.new('The Hobbit', 'J.R.R. Tolkien', []) 
-          expect(bk.title).not_to be_nil
-          expect(bk.title).to be_a(String)
-       end
+describe Book do
+  context 'When testing the Book class' do
+    it 'A newly created book has a title of type String' do
+      bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
+      expect(bk.title).not_to be_nil
+      expect(bk.title).to be_a(String)
+    end
 
-       it 'A newly created book has an author of type String' do
-            bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
-            expect(bk.author).not_to be_nil
-            expect(bk.author).to be_a(String)
-       end
+    it 'A newly created book has an author of type String' do
+      bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
+      expect(bk.author).not_to be_nil
+      expect(bk.author).to be_a(String)
+    end
 
-       it 'A newly created book has a rentals property of type Array' do
-            bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
-            expect(bk.rentals).not_to be_nil
-            expect(bk.rentals).to be_a(Array)
-       end
+    it 'A newly created book has a rentals property of type Array' do
+      bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
+      expect(bk.rentals).not_to be_nil
+      expect(bk.rentals).to be_a(Array)
+    end
 
-       it 'The rentals property of a book is an Array of objects from class Rental' do
-            bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
-            bk2 = Book.new('Book1', 'Title1', ['Rental1', 'Rental2'])
+    it 'The rentals property of a book is an Array of objects from class Rental' do
+      bk = Book.new('The Hobbit', 'J.R.R. Tolkien', [])
+      bk2 = Book.new('Book1', 'Title1', %w[Rental1 Rental2])
 
-            expect(bk.rentals).to all(be_a(Rental))
-            expect(bk2.rentals.all? { |rental| rental.is_a?(Rental) }).to be false
-       end
-    end 
- end
+      expect(bk.rentals).to all(be_a(Rental))
+      expect(bk2.rentals.all? { |rental| rental.is_a?(Rental) }).to be false
+    end
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,16 +2,15 @@ require_relative '../student'
 require_relative '../classroom'
 
 describe Student do
-    context 'When testing the Student class' do 
-
-        it 'A Student is also a Person (inheritance)' do
-            student = Student.new(24, 'John Doe')
-            expect(student).to be_kind_of(Person)
-        end
-
-        it 'A Student has a classroom property of type Classroom' do 
-            student = Student.new(24, 'Classroom1', 'John Doe', false)
-            expect(student.classroom).to be_a(Classroom)
-        end
+  context 'When testing the Student class' do
+    it 'A Student is also a Person (inheritance)' do
+      student = Student.new(24, 'John Doe')
+      expect(student).to be_kind_of(Person)
     end
+
+    it 'A Student has a classroom property of type Classroom' do
+      student = Student.new(24, 'Classroom1', 'John Doe', false)
+      expect(student.classroom).to be_a(Classroom)
+    end
+  end
 end
