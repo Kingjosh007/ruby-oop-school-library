d8b5d23779f8582e491c9b445bd14a04b3b54c94
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-09 15:12:15 +0100

    Write tests for Person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../person'
+
+describe Person do 
+    context 'When testing the Person class' do 
+
+        it 'A person created with only one parameter has a name property set to "Unknown"' do 
+            person = Person.new(24)
+            expect(person.name).to eq('Unknown')
+        end
+
+        it 'A person created with only two parameters has a parent_permission property set to true' do 
+            person = Person.new(54, 'John Doe')
+            expect(person.parent_permission).to be true
+        end
+    end
+end
